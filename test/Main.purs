847212b5af5f9f07cf6f1f3cecdec3463541cdbd
(cdep)illabout <cdep.illabout@gmail.com>
2015-12-27 02:07:09 +0900

    Add tests for functor laws.

--- test/Main.purs
+++ test/Main.purs
@@ -2,15 +2,17 @@ module Test.Main where
 
 import Prelude
 
+import Data.Functor (($>))
 import Control.Monad.Aff
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Random (RANDOM)
-import Control.Monad.Eff.Ref (REF, Ref)
+import Control.Monad.Eff.Ref (REF, Ref, readRef, writeRef, newRef)
 import Data.Monoid (class Monoid)
-import Data.Tuple (Tuple)
+import Data.String (length)
+import Data.Tuple (Tuple(..))
 import Test.QuickCheck.Arbitrary (class Arbitrary, arbitrary)
 import Test.Spec (describe, pending, it, prop)
 import Test.Spec.Runner (Process, run)
@@ -19,19 +21,40 @@ import Test.Spec.Reporter.Console (consoleReporter)
 
 import Control.Monad.Logger (Logger(..), runLogger')
 
-runLoggerEff :: forall eff w a . (Monoid w) => Logger eff w a -> Aff (ref :: REF | eff) (Tuple a w)
-runLoggerEff = liftEff <<< runLogger'
+runLoggerEff :: forall eff w a
+              . Logger eff w a
+             -> w
+             -> Aff (ref :: REF | eff) (Tuple a w)
+runLoggerEff (Logger logger) w = do
+    ref <- liftEff $ newRef w
+    a <- liftEff $ logger ref
+    w' <- liftEff $ readRef ref
+    return $ Tuple a w
 
 main :: forall eff . Eff (process :: Process, console :: CONSOLE, random :: RANDOM, err :: EXCEPTION, ref :: REF | eff) Unit
 main = run [consoleReporter] do
-    describe "What is it?" do
-        it "awesome" do
-            let isAwesome = true
-            isAwesome `shouldEqual` true
-
     describe "Control.Monad.Logger" do
-        it "follows functor law id" $ do
-            let logger = Logger \(ref :: Ref String) -> return (3 :: Int)
-            res1 <- runLoggerEff (map id logger)
-            res2 <- runLoggerEff logger
-            res1 `shouldEqual` res2
+        describe "follows functor laws" $ do
+            it "map id = id" $ do
+                let logger1 = Logger \ref -> writeRef ref 10 $> 3
+                res1 <- runLoggerEff (map id logger1) 4
+                res2 <- runLoggerEff logger1 4
+                res1 `shouldEqual` res2
+
+                let logger2 = Logger \ref -> pure 3
+                res3 <- runLoggerEff (map id logger2) 5
+                res4 <- runLoggerEff logger2 5
+                res3 `shouldEqual` res4
+
+            it "map (p <<< q) = map p <<< map q" $ do
+                let p = length
+                    q = show
+                let logger1 = Logger \ref -> writeRef ref 1000 $> 100
+                res1 <- runLoggerEff (map (p <<< q) logger1) 10
+                res2 <- runLoggerEff ((map p <<< map q) logger1) 10
+                res1 `shouldEqual` res2
+
+                let logger2 = Logger \ref -> pure 100
+                res3 <- runLoggerEff (map (p <<< q) logger2) 10
+                res4 <- runLoggerEff ((map p <<< map q) logger2) 10
+                res3 `shouldEqual` res4
